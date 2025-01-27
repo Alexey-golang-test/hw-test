63a53794d9063018755cca47b3ad32046192f06d
Alexey-golang-test <alexeymoscow0952@gmail.com>
2025-01-27 23:41:31 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,69 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+// Run starts tasks in nn goroutines and stops its work when receiving mm errors from tasks.
+func Run(tasks []Task, nn, mm int) error {
+	if nn < 1 {
+		// Защита входных данных. Если кол-во горутин менее 1, то будет работать в 1 поток
+		nn = 1
+	}
+
+	if mm <= 0 {
+		// Если кол-во ошибок меньше либо равно 0, то ошибок быть не должно
+		mm = 0
+	}
+
+	ch := make(chan Task, nn)
+	var wg sync.WaitGroup
+	var errAtomic atomic.Int64
+
+	// Запуск горутин на выполнение: слушают канал, выполняют таск, фиксируют ошибки (если есть)
+	for ii := 0; ii < nn; ii++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for {
+				// Читаем канал
+				tt, ok := <-ch
+
+				// Канал закрыт -> выход
+				if !ok {
+					return
+				}
+
+				// Лучше сразу проверить кол-во ошибок, чтобы не делать лишнюю таску
+				if ee := errAtomic.Load(); ee >= int64(mm) && ee > 0 {
+					return
+				}
+
+				// Выполнение таски, нужно увеличивать счетчик ошибок
+				if err := tt(); err != nil {
+					errAtomic.Add(1)
+				}
+			}
+		}()
+	}
+
+	// Отправка тасок в канал
+	for _, tt := range tasks {
+		ch <- tt
+	}
+	close(ch)
+
+	wg.Wait()
+
+	if ee := errAtomic.Load(); ee >= int64(mm) && ee > 0 {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,84 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("некорректное кол-во горутин", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := -5
+		maxErrorsCount := 1
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "не все таски завершены")
+	})
+
+	t.Run("допустимое кол-во ошибок 0, нет ошибок", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 100
+		maxErrorsCount := 0
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.NoError(t, err)
+
+		require.Equal(t, runTasksCount, int32(tasksCount), "не все таски завершены")
+	})
+
+	t.Run("допустимое кол-во ошибок 0, есть ошибка", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+			err := fmt.Errorf("error from task %d", i)
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 100
+		maxErrorsCount := 0
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	})
 }
