912b8017e8080765ac87d39a57fc8fd4563acbef
Alexey-golang-test <alexeymoscow0952@gmail.com>
2025-01-28 22:26:26 +0300

    HW6 draft commit

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// Эта функция слушает два канала. Служит как переходник для Stage
+	// С одной стороны надо переложить In в Out
+	// С другой стороны надо прерывать работу через done-канал
+	stageFunc := func(done In, in In) Out {
+		resChan := make(Bi)
+
+		go func() {
+			defer close(resChan)
+
+			for ii := range in {
+				select {
+				case <-done:
+					return
+				case resChan <- ii:
+				}
+			}
+		}()
+
+		return resChan
+	}
+
+	// Последовательный запуск стадий пайплайна.
+	for _, ss := range stages {
+		// in переопределяется, это не ошибка.
+		// На каждой итерации цикла исходящий канал функции сохраняется в переменную,
+		// и уже на следующей итерации является входящим каналом в функцию
+		// Это аналог рекурсивного вызова ss(stageFunc(done, ss(stageFunc(done, ss(stageFunc(done, ... и т.д.))))))
+		in = ss(stageFunc(done, in))
+	}
+
+	return in
 }
